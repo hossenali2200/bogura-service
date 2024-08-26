0516ec7e6fff05b9fa750d70feb4a62b48ae9cce
hossenali2200 <hosenali2200@gmail.com>
2024-08-26 12:53:18 +0600

    Footer section

--- Bogura-services/src/components/PopularHome.jsx
+++ Bogura-services/src/components/PopularHome.jsx
@@ -55,6 +55,59 @@ const PopularHome = () => {
             </div>
 
         </div>
+
+        <div className='mt-8'>
+        <h2 className='font-bold text-3xl mb-4'>Keep your home in tip-top shape</h2>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            <div>
+                
+                <div>
+                    <div className='flex justify-between border-2 p-3 rounded-t-lg'>
+                        <h2 className=' font-bold text-lg'>Faucets, Fixtures & Pipes Repair or Replace</h2>
+                        <div className='flex gap-2 '>
+                        <FaStar  className=' text-lg mt-1 text-red-500' />
+                        <p> 4.5 (300)</p>
+                        </div>
+                    </div>
+                    <div className='relative  '>
+                        <img className=' h-[300px] w-full ' src="https://media.istockphoto.com/id/1440019701/photo/close-up-of-plumber-repairing-sink-with-tool-in-bathroom.webp?s=2048x2048&w=is&k=20&c=5m-JfCrZBaSidsv4kUo5my6keChi4e5wIsaF832h-88=" alt="" />
+                        <p className=' absolute top-56 bg-white text-black p-2 rounded-lg left-4 '>From $200/Book Now</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div className='flex justify-between border-2 p-7 rounded-t-lg'>
+                        <h2 className=' font-bold text-lg'>Roof Installation And Repair</h2>
+                        <div className='flex gap-2 '>
+                        <FaStar  className=' text-lg mt-1 text-red-500' />
+                        <p> 4.5 (300)</p>
+                        </div>
+                    </div>
+                    <div className='relative  '>
+                        <img className=' h-[300px] w-full ' src="https://plus.unsplash.com/premium_photo-1661899263522-753a88ef6895?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cm9vZiUyMHJlcGFpcnxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+                        <p className=' absolute top-56 bg-white text-black p-2 rounded-lg left-4 '>From $200/Book Now</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div className='flex justify-between border-2 p-7  rounded-t-lg'>
+                        <h2 className=' font-bold text-lg'>Bathroom Remodel</h2>
+                        <div className='flex gap-2 '>
+                        <FaStar  className=' text-lg mt-1 text-red-500' />
+                        <p> 4.5 (300)</p>
+                        </div>
+                    </div>
+                    <div className='relative  '>
+                        <img className=' h-[300px] w-full ' src="https://images.unsplash.com/photo-1552321554-5fefe8c9ef14?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhdGhyb29tJTIwcmVtb2RlbHxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+                        <p className=' absolute top-56 bg-white text-black p-2 rounded-lg left-4 '>From $200/Book Now</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        </div>
     </div>
   )
 }
--- Bogura-services/src/pages/Home.jsx
+++ Bogura-services/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import PopularProject from '../components/PopularProject'
 import PopularHome from '../components/PopularHome'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <PopularProject/>
       <PopularHome/>
+      <Banner/>
     </div>
   )
 }
